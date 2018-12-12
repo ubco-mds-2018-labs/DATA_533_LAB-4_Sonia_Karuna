68388e245d03ba3f5cea00c64a9d16a503108a1c
Karuna <42846528+karunalakhani@users.noreply.github.com>
2018-12-12 13:58:36 +0530

    Add files via upload

--- Mainbot/Subpackage2/config.py
+++ Mainbot/Subpackage2/config.py
@@ -4,17 +4,23 @@
 # In[2]:
 
 
-def config(env):
-    if env=='SLACK_TOKEN':
-        SLACK_TOKEN='xoxb-493323995142-491822670659-tLQJy2eckXUBgFKhET9eHvO4'
-        return SLACK_TOKEN
-    elif env=='BOTNAME':
-        botname = 'Chatbot'
-        return botname    
-    elif env =='BOT_ID':
-        BOT_ID = 'AEFT61GF4'
-        return BOT_ID
-
+try:
+    
+    def config(env):
+        try:
+            if env=='SLACK_TOKEN':
+                SLACK_TOKEN='xoxb-493323995142-491822670659-tLQJy2eckXUBgFKhET9eHvO4'
+                return SLACK_TOKEN
+            elif env=='BOTNAME':
+                botname = 'Chatbot'
+                return botname    
+            elif env =='BOT_ID':
+                BOT_ID = 'AEFT61GF4'
+                return BOT_ID
+        except:
+            print("Function returned nothing")
+except:
+    print("Configuration not defined")
 
 
         
