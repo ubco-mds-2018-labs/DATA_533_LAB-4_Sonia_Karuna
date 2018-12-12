2d3656bd8852a4d7f21954b562c3ad03a98b81bf
Karuna <42846528+karunalakhani@users.noreply.github.com>
2018-12-12 15:57:29 +0530

    Update testing_bot_questions.py

--- testing_bot_questions.py
+++ testing_bot_questions.py
@@ -23,6 +23,7 @@ class TestBot(unittest.TestCase):
     def setUp(self): 
         self.command = "How are you"
         self.command1 = "What do you do"
+        self.command2 = "Hi"
         
     def tearDown(self):
         print('Tear Down')
@@ -31,12 +32,13 @@ class TestBot(unittest.TestCase):
     def test_questions(self):
         cmd = bt.questions(self.command)
         cmd1 = bt.questions(self.command1)
+        cmd2 = bt.questions(self.command2)
         print(self.assertEqual(cmd,"I am good, how are you doing today?"))
         print(self.assertTrue(cmd,"I am good, how are you doing today?"))
         print(self.assertEqual(cmd1,"I try to find out the answers for your questions, go ahead and ask me about MDS program"))
         print(self.assertTrue(cmd1,"I try to find out the answers for your questions, go ahead and ask me about MDS program"))
         print(self.assertNotEqual(cmd1,"I am good, how are you doing today?"))
-        #print(self.assertNotEqual(cmd1,"I try to find out the answers for your questions, go ahead and ask me about MDS program"))
+        print(self.assertNotEqual(cmd2,"I try to find out the answers for your questions, go ahead and ask me about MDS program"))
  #other than the last print statement, it passes test for rest all, last one is written for fail intentionally.
 unittest.main(argv=[''], verbosity=2, exit=False)
 
