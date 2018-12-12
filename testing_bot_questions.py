2def2d73e2fe8c7b89bf2e64fa2db2f0a6b8e12b
soniagrewal17 <42872374+soniagrewal17@users.noreply.github.com>
2018-12-12 00:46:40 -0800

    Update testing_bot_questions.py

--- testing_bot_questions.py
+++ testing_bot_questions.py
@@ -36,6 +36,6 @@ class TestBot(unittest.TestCase):
         print(self.assertTrue(cmd1,"I try to find out the answers for your questions, go ahead and ask me about MDS program"))
         print(self.assertNotEqual(cmd1,"I am good, how are you doing today?"))
         #print(self.assertNotEqual(cmd1,"I try to find out the answers for your questions, go ahead and ask me about MDS program"))
-# other than the last print statement, it passes test for rest all, last one is written for fail intentionally.
+ #other than the last print statement, it passes test for rest all, last one is written for fail intentionally.
 unittest.main(argv=[''], verbosity=2, exit=False)
 
